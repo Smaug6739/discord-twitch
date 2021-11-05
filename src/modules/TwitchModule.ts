d5e1260d4ab88939f2afb274ebdd1f7c8e8b5a5a
Smaug6739 <veauville.raphael@gmail.com>
2021-11-05 11:11:16 +0100

    Fix image

--- src/modules/TwitchModule.ts
+++ src/modules/TwitchModule.ts
@@ -1,6 +1,6 @@
 import { getAllStreamer, updateStreamer } from "../services/TwitchService";
 import { getStream } from "../utils/TwitchAuth";
-import { MessageEmbed } from "discord.js";
+import { MessageEmbed, MessageAttachment } from "discord.js";
 import type { TextChannel } from "discord.js";
 
 module.exports = async (client) => {
@@ -12,7 +12,7 @@ module.exports = async (client) => {
         if (streamers.length < 1) return console.log("Aucun streamer");
         for (const streamer of streamers) {
             const stream = await getStream(client, streamer.twitchUserID);
-            if(stream && stream === "no data") return;
+            if (stream && stream === "no data") return;
 
             if (!stream) return await updateStreamer(streamer, false);
 
@@ -21,16 +21,19 @@ module.exports = async (client) => {
             if (streamer.announced === false) {
                 //@ts-ignore
                 const chan: TextChannel = client.channels.cache.get("886285800671694888");
-
+                const attachment = new MessageAttachment(
+                    stream.thumbnail_url.replace("{width}", "1920").replace("{height}", "1080", "thumbnail.png")
+                );
                 const embed = new MessageEmbed()
                     .setAuthor(stream.user_name, streamer.profileImageUrl)
+                    .attachFiles([attachment])
                     .setThumbnail(streamer.profileImageUrl)
                     .setTitle(stream.title)
                     .setURL(`https://www.twitch.tv/${stream.login}`)
                     .addField("Game", stream.game_name, true)
                     .addField("Viewers", `${stream.viewer_count}`, true)
                     .setColor(9521151)
-                    .setImage(stream.thumbnail_url.replace("{width}", "1920").replace("{height}", "1080"));
+                    .setImage(`attachment://thumbnail.png`);
                 chan.send("Salut Lucifer lance un stream @everyone", embed);
                 await updateStreamer(streamer, true, started);
             }
